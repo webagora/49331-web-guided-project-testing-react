8c1ac761fd0da2379cd736960ba5fb60c87a229b
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-12-13 13:20:59 -0500

    lecture work-in-progress

--- src/components/MissionForm.test.js
+++ src/components/MissionForm.test.js
@@ -40,11 +40,9 @@ test("executes getData when the button is clicked", ()=> {
     //Act : get the button. click the button
     const button = screen.getByRole('button');
     userEvent.click(button);
-    userEvent.click(button);
-    userEvent.click(button);
 
     //Assert : ????
     console.log(mockGetData.mock);
-    
+    expect(mockGetData.mock.calls.length).toBe(1);
 });
 
