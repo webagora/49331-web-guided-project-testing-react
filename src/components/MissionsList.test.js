a5ae85c172a431dd29ea28990758ceca203d16db
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-12-13 13:54:26 -0500

    lecture work-in-progress

--- src/components/MissionsList.test.js
+++ src/components/MissionsList.test.js
@@ -7,5 +7,15 @@ test("renders without errors", ()=> {
 });
 
 test("renders no missions and then renders 3 missions on rerendering", ()=> {
+    //Arrange 1
+    render(<MissionsList mission={[]}/>);
+
+    //Act 1
+    
+    //Assert 1
+
+    //Arrange 2
+    //Act 3
+    //Assert 3
     
 });
