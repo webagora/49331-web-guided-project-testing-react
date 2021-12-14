e5ce58c70df6d9a3d16c7f95c5e9112aeb00e439
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-12-13 17:16:46 -0800

    MissionsList.test.test.js - renders no missions

--- src/components/MissionsList.test.js
+++ src/components/MissionsList.test.js
@@ -5,3 +5,15 @@ import MissionsList from "./MissionsList"
 test ("renders without errors", () => {
     render ( <MissionsList missions = { [] } />)
 })
+
+test("renders no missions and then renders 3 missions on rerendering", ()=> { 
+    ///Arrange 1
+    render(<MissionsList missions={[]}/>);
+
+    //Act 1
+    let missions = screen.queryAllByTestId('mission');
+
+    //Assert 1
+    expect(missions).toHaveLength(0);
+    
+})
