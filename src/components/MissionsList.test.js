c62ccebbd97ec1329dd7f87f23587da302e18225
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-12-13 17:24:43 -0800

    MissionsList.test.test.js - rerender to renders 3 missions

--- src/components/MissionsList.test.js
+++ src/components/MissionsList.test.js
@@ -8,9 +8,8 @@ test ("renders without errors", () => {
 
 test("renders no missions and then renders 3 missions on rerendering", ()=> { 
     ///Arrange 1
-    const renderStuff = render(<MissionsList missions={[]}/>);
+    const { rerender } = render(<MissionsList missions={[]}/>);
 
-    console.log('renderStuff: ', renderStuff);
     //Act 1
     let missions = screen.queryAllByTestId('mission');
 
@@ -18,9 +17,15 @@ test("renders no missions and then renders 3 missions on rerendering", ()=> {
     expect(missions).toHaveLength(0);
     
     //Arrange 2
+    rerender (<MissionsList missions = {[
+        {mission_id:1, mission_name: "Test 1"},
+        {mission_id:2, mission_name: "Test 2"},
+        {mission_id:3, mission_name: "Test 3"}
+    ]}  />)
     
     //Act 2
-    
+    missions = screen.queryAllByTestId('mission');
+
     //Assert 2
-    
+    expect(missions).toHaveLength(3);
 })
